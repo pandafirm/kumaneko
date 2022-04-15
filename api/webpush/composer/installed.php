fde5025b445722101571b88897f23e87444d19f3
pandafirm <97499949+pandafirm@users.noreply.github.com>
2022-04-15 10:03:23 +0900

    Add files via upload

---
+++ api/webpush/composer/installed.php
@@ -0,0 +1,198 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dce2c34c23035cfe8911f965e4143db1a51c5bdf',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dce2c34c23035cfe8911f965e4143db1a51c5bdf',
+    ),
+    'brick/math' => 
+    array (
+      'pretty_version' => '0.9.3',
+      'version' => '0.9.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ca57d18f028f84f777b2168cd1911b0dee2343ae',
+    ),
+    'fgrosse/phpasn1' => 
+    array (
+      'pretty_version' => 'v2.4.0',
+      'version' => '2.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eef488991d53e58e60c9554b09b1201ca5ba9296',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.4.2',
+      'version' => '7.4.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ac1ec1cd9b5624694c3a40be801d94137afb12b4',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.5.1',
+      'version' => '1.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe752aedc9fd8fcca3fe7ad05d419d32998a06da',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '2.2.1',
+      'version' => '2.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c94a94f120803a18554c1805ef2e539f8285f9a2',
+    ),
+    'minishlink/web-push' => 
+    array (
+      'pretty_version' => 'v7.0.0',
+      'version' => '7.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8b023f2f5c6df846c0cfb87e13fb7ddf5e62c340',
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12ac7fcd07e5b077433f5f2bee95b3a771bf61be',
+    ),
+    'psr/http-factory-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'spomky-labs/base64url' => 
+    array (
+      'pretty_version' => 'v2.0.4',
+      'version' => '2.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7752ce931ec285da4ed1f4c5aa27e45e097be61d',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.5.1',
+      'version' => '2.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8b495ea28c1d97b5e0c121748d6f9b53d075c66',
+    ),
+    'web-token/jwt-core' => 
+    array (
+      'pretty_version' => 'v2.2.11',
+      'version' => '2.2.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53beb6f6c1eec4fa93c1c3e5d9e5701e71fa1678',
+    ),
+    'web-token/jwt-key-mgmt' => 
+    array (
+      'pretty_version' => 'v2.2.11',
+      'version' => '2.2.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0b116379515700d237b4e5de86879078ccb09d8a',
+    ),
+    'web-token/jwt-signature' => 
+    array (
+      'pretty_version' => 'v2.2.11',
+      'version' => '2.2.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '015b59aaf3b6e8fb9f5bd1338845b7464c7d8103',
+    ),
+    'web-token/jwt-signature-algorithm-ecdsa' => 
+    array (
+      'pretty_version' => 'v2.2.11',
+      'version' => '2.2.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '44cbbb4374c51f1cf48b82ae761efbf24e1a8591',
+    ),
+    'web-token/jwt-util-ecc' => 
+    array (
+      'pretty_version' => 'v2.2.11',
+      'version' => '2.2.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '915f3fde86f5236c205620d61177b9ef43863deb',
+    ),
+  ),
+);
