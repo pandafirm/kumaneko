7ae57d42e7abd0b8bfff93ec49ca6dfa739aa2f5
pandafirm <97499949+pandafirm@users.noreply.github.com>
2022-04-15 09:55:41 +0900

    Add files via upload

---
+++ api/notify.php
@@ -0,0 +1,84 @@
+<?php
+/*
+* PandaFirm-PHP-Module "notify.php"
+* Version: 1.0
+* Copyright (c) 2020 TIS
+* Released under the MIT License.
+* http://pandafirm.jp/license.txt
+*/
+require_once(dirname(__FILE__)."/lib/base.php");
+class clsRequest extends clsBase
+{
+	/* valiable */
+	private $body;
+	private $response;
+	/* constructor */
+	public function __construct()
+	{
+		$this->response=[];
+	}
+	/* methods */
+	protected function GET()
+	{
+		$this->body=$_GET;
+		$file=dirname(__FILE__)."/webpush/keys.json";
+		if (file_exists($file))
+		{
+			$keys=json_decode(mb_convert_encoding(file_get_contents($file),'UTF8','ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'),true);
+			if (is_array($keys))
+			{
+				$this->response["key"]=$keys["publicKey"];
+				header("HTTP/1.1 200 OK");
+				header('Content-Type: application/json; charset=utf-8');
+				echo json_encode($this->response,JSON_UNESCAPED_UNICODE);
+				exit(0);
+			}
+			else $this->callrequesterror(500,"Key file is corrupted");
+		}
+		else $this->callrequesterror(500,"Key file does not exist");
+	}
+	protected function POST()
+	{
+		$this->body=json_decode(mb_convert_encoding(file_get_contents('php://input'),'UTF8','ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'),true);
+		if (!isset($this->body["endpoint"])) $this->callrequesterror(400);
+		if (!isset($this->body["publicKey"])) $this->callrequesterror(400);
+		if (!isset($this->body["authToken"])) $this->callrequesterror(400);
+		$file=dirname(__FILE__)."/webpush/subscriptions.json";
+		$endpoint=$this->body["endpoint"];
+		$publicKey=$this->body["publicKey"];
+		$authToken=$this->body["authToken"];
+		$subscriptions=(file_exists($file))?json_decode(mb_convert_encoding(file_get_contents($file),'UTF8','ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'),true):[];
+		if (count(array_filter($subscriptions,function($value) use ($endpoint,$publicKey,$authToken){return $value["endpoint"]==$endpoint && $value["publicKey"]==$publicKey && $value["authToken"]==$authToken;}))==0)
+		{
+			$subscriptions[]=[
+				"endpoint"=>$endpoint,
+				"publicKey"=>$publicKey,
+				"authToken"=>$authToken
+			];
+			file_put_contents($file,json_encode($subscriptions));
+		}
+		header("HTTP/1.1 200 OK");
+		header('Content-Type: application/json; charset=utf-8');
+		echo json_encode($this->response,JSON_UNESCAPED_UNICODE);
+		exit(0);
+	}
+	protected function PUT()
+	{
+		$this->body=json_decode(mb_convert_encoding(file_get_contents('php://input'),'UTF8','ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'),true);
+		if (!isset($this->body["payload"])) $this->callrequesterror(400);
+		if (!isset($this->body["subject"])) $this->callrequesterror(400);
+		if (substr(php_uname(),0,7)=="Windows") pclose(popen("start /B php notify_processing.php ".$this->body["payload"]." ".$this->body["subject"],"r"));
+		else exec("nohup php notify_processing.php ".$this->body["payload"]." ".$this->body["subject"]." > /dev/null &");
+		header("HTTP/1.1 200 OK");
+		header('Content-Type: application/json; charset=utf-8');
+		echo json_encode($this->response,JSON_UNESCAPED_UNICODE);
+		exit(0);
+	}
+	protected function DELETE()
+	{
+		$this->callrequesterror(400);
+	}
+}
+$cls_request=new clsRequest();
+$cls_request->checkmethod();
+?>
---
+++ api/notify_processing.php
@@ -0,0 +1,72 @@
+<?php
+/*
+* PandaFirm-PHP-Module "notify_processing.php"
+* Version: 1.0
+* Copyright (c) 2020 TIS
+* Released under the MIT License.
+* http://pandafirm.jp/license.txt
+*/
+global $argv;
+require_once(dirname(__FILE__)."/webpush/autoload.php");
+use Minishlink\WebPush\WebPush;
+use Minishlink\WebPush\Subscription;
+register_shutdown_function(function(){
+	$error=error_get_last();
+	if ($error===null) return;
+	file_put_contents("./notify_processing.error",$error["message"]);
+});
+if (count($argv)>1)
+{
+	try
+	{
+		$payload=$argv[1];
+		$subject=$argv[2];
+		$file=dirname(__FILE__)."/webpush/keys.json";
+		if (file_exists($file))
+		{
+			$keys=json_decode(mb_convert_encoding(file_get_contents($file),'UTF8','ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'),true);
+			if (is_array($keys))
+			{
+				$file=dirname(__FILE__)."/webpush/subscriptions.json";
+				if (file_exists($file))
+				{
+					$subscriptions=json_decode(mb_convert_encoding(file_get_contents($file),'UTF8','ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'),true);
+					if (is_array($subscriptions))
+					{
+						$webPush=new WebPush([
+							"VAPID"=>[
+								"subject"=>$subject,
+								"publicKey"=>$keys["publicKey"],
+								"privateKey"=>$keys["privateKey"]
+							]
+						]);
+						foreach ($subscriptions as $subscription)
+						{
+							$webPush->queueNotification(
+								Subscription::create([
+									"endpoint"=>$subscription["endpoint"],
+									"publicKey"=>$subscription["publicKey"],
+									"authToken"=>$subscription["authToken"]
+								]),
+								$payload
+							);
+						}
+						foreach ($webPush->flush() as $report)
+							if (!$report->isSuccess())
+							{
+								$endpoint=$report->getRequest()->getUri()->__toString();
+								$subscriptions=array_filter($subscriptions,function($value) use ($endpoint){return $value["endpoint"]!=$endpoint;});
+							}
+					}
+					file_put_contents($file,json_encode($subscriptions));
+				}
+			}
+		}
+	}
+	catch (Exception $e)
+	{
+		file_put_contents("./notify_processing.error",$e->getMessage());
+	}
+}
+exit(0);
+?>
