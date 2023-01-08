165eef54f388ed941bc5475b32e5161c98458fc7
pandafirm <97499949+pandafirm@users.noreply.github.com>
2023-01-08 11:15:31 +0900

    Add files via upload

--- api/google/sheet.php
+++ api/google/sheet.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * PandaFirm-PHP-Module "sheet.php"
-* Version: 1.1.5
+* Version: 1.1.6
 * Copyright (c) 2020 Pandafirm LLC
 * Distributed under the terms of the GNU Lesser General Public License.
 * https://opensource.org/licenses/LGPL-2.1
