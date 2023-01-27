614ef39cf0f743ef9a4890b8aaffdd850a83ff4e
pandafirm <97499949+pandafirm@users.noreply.github.com>
2023-01-27 11:42:15 +0900

    Add files via upload

--- api/google/service.php
+++ api/google/service.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * PandaFirm-PHP-Module "service.php"
-* Version: 1.1.7
+* Version: 1.2.0
 * Copyright (c) 2020 Pandafirm LLC
 * Distributed under the terms of the GNU Lesser General Public License.
 * https://opensource.org/licenses/LGPL-2.1
