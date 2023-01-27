77856a8f2054d96a93ea0a79826232ede963497c
pandafirm <97499949+pandafirm@users.noreply.github.com>
2023-01-27 11:39:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /*
 * FileName "sw.js"
-* Version: 1.1.7
+* Version: 1.2.0
 * Copyright (c) 2020 Pandafirm LLC
 * Distributed under the terms of the GNU Lesser General Public License.
 * https://opensource.org/licenses/LGPL-2.1
