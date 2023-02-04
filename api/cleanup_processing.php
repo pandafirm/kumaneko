9127c13facc3e4bf408bfd97f2996ed555adfcc5
pandafirm <97499949+pandafirm@users.noreply.github.com>
2023-02-04 11:58:20 +0900

    Add files via upload

--- api/cleanup_processing.php
+++ api/cleanup_processing.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * PandaFirm-PHP-Module "cleanup_processing.php"
-* Version: 1.2.0
+* Version: 1.2.1
 * Copyright (c) 2020 Pandafirm LLC
 * Distributed under the terms of the GNU Lesser General Public License.
 * https://opensource.org/licenses/LGPL-2.1
