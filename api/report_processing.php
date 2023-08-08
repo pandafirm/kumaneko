54649354c293776780bee475e0ca3efc5aae4f50
pandafirm <97499949+pandafirm@users.noreply.github.com>
2023-08-08 11:06:44 +0900

    Add files via upload

--- api/report_processing.php
+++ api/report_processing.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * PandaFirm-PHP-Module "report_processing.php"
-* Version: 1.3.6
+* Version: 1.3.7
 * Copyright (c) 2020 Pandafirm LLC
 * Distributed under the terms of the GNU Lesser General Public License.
 * https://opensource.org/licenses/LGPL-2.1
