4bf0e67bb152fe489343039ed7f2de2430e78fb1
pandafirm <97499949+pandafirm@users.noreply.github.com>
2023-09-22 11:05:48 +0900

    Add files via upload

--- api/crosstab.php
+++ api/crosstab.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * PandaFirm-PHP-Module "crosstab.php"
-* Version: 1.3.9
+* Version: 1.3.10
 * Copyright (c) 2020 Pandafirm LLC
 * Distributed under the terms of the GNU Lesser General Public License.
 * https://opensource.org/licenses/LGPL-2.1
