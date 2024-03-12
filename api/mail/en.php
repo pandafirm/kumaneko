1c3ccd62185e74b410c258955f4e5b73a0139441
pandafirm <97499949+pandafirm@users.noreply.github.com>
2024-03-12 16:17:25 +0900

    Add files via upload

--- api/mail/en.php
+++ api/mail/en.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * PandaFirm-PHP-Module "mail/en.php"
-* Version: 1.5.3
+* Version: 1.6.0
 * Copyright (c) 2020 Pandafirm LLC
 * Distributed under the terms of the GNU Lesser General Public License.
 * https://opensource.org/licenses/LGPL-2.1
