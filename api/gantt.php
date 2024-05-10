0f118e45a5a1d5dbea7a90fcf26570d76e7bd4c8
pandafirm <97499949+pandafirm@users.noreply.github.com>
2024-05-10 16:00:56 +0900

    Add files via upload

--- api/gantt.php
+++ api/gantt.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * PandaFirm-PHP-Module "gantt.php"
-* Version: 1.6.2
+* Version: 1.6.3
 * Copyright (c) 2020 Pandafirm LLC
 * Distributed under the terms of the GNU Lesser General Public License.
 * https://opensource.org/licenses/LGPL-2.1
